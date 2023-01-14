d82decd7b2885dea9d840e36707e7ca69005df3f
Fernando Schwalb Costa <fschwalb@gmail.com>
2023-01-15 00:52:45 +0100

    First handleSubmit @ Formulario.jsx

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -5,6 +5,14 @@ const Formulario = () => {
 
     const [nombre, setNombre] = useState('');
 
+    const handleSubmit = ( e ) => {
+
+        e.preventDefault()
+
+        console.log('enviando formulario');
+
+    }
+
     return (
         <div className="md:w-1/2 lg:w-2/5">
             <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
@@ -14,7 +22,10 @@ const Formulario = () => {
                 <span className="text-indigo-600 font-bold">Administralos</span>
             </p>
 
-            <form className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+            <form
+                onSubmit={ handleSubmit }
+                className="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
+            >
                 <div className="mb-5">
                     <label htmlFor="mascota" className="block text-gray-700 uppercase font-bold">
                         Nombre Mascota
@@ -25,7 +36,7 @@ const Formulario = () => {
                         type="text"
                         placeholder="Nombre de la Mascota"
                         className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
-                        value={nombre}
+                        value={ nombre }
                         onChange={ (e) => setNombre(e.target.value) }
                     />
                 </div>
