32ab7fcc2c861673bda8793c95aef548c408f9ac
Fernando Schwalb Costa <fschwalb@gmail.com>
2023-01-15 00:44:04 +0100

    First useState @ Formulario.jsx

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,4 +1,9 @@
+import { useState, useEffect } from 'react'
+
+
 const Formulario = () => {
+
+    const [nombre, setNombre] = useState('');
 
     return (
         <div className="md:w-1/2 lg:w-2/5">
@@ -20,6 +25,8 @@ const Formulario = () => {
                         type="text"
                         placeholder="Nombre de la Mascota"
                         className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                        value={nombre}
+                        onChange={ (e) => setNombre(e.target.value) }
                     />
                 </div>
 
