3e769b228c14215857e4e07f6a673bc0f69fd039
Fernando Schwalb Costa <fschwalb@gmail.com>
2023-01-16 01:58:27 +0100

    Error @ Forulario.jsx Started

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -52,7 +52,7 @@ const Formulario = ({ pacientes, setPacientes }) => {
         <div className="md:w-1/2 lg:w-2/5 mx-5">
             <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
 
-            <p className="text-lg mt-5 text-center mb-10">
+            <p className="text-xl mt-5 text-center mb-10">
                 Añade Pacientes y {''}
                 <span className="text-indigo-600 font-bold">Administralos</span>
             </p>
