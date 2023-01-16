c9ccd3566d7c33cf51f2405a9087ba044871fc3f
Fernando Schwalb Costa <fschwalb@gmail.com>
2023-01-16 19:28:17 +0100

    ListadoPacientes.jsx Subtitulo dinamico

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -5,20 +5,35 @@ const ListadoPacientes = ({ pacientes }) => {
     return (
 
         <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
-            <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
 
-            <p className="text-xl mt-5 mb-10 text-center">
-                Administra tus {''}
-                <span className="text-indigo-600 font-bold text-xl">Pacientes y Citas</span>
-            </p>
+            { pacientes && pacientes.length ? (
+                <>
+                    <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
 
-            { pacientes.map( paciente => (
-                <Paciente
-                    key={ paciente.id }
-                    paciente={ paciente }
-                />
-            ))}
+                    <p className="text-xl mt-5 mb-10 text-center">
+                        Administra tus {''}
+                        <span className="text-indigo-600 font-bold text-xl">Pacientes y Citas</span>
+                    </p>
 
+                    { pacientes.map( paciente => (
+                        <Paciente
+                            key={ paciente.id }
+                            paciente={ paciente }
+                        />
+                    ))}
+                </>
+
+
+            ) : (
+                <>
+                    <h2 className="font-black text-3xl text-center">No hay pacientes</h2>
+
+                    <p className="text-xl mt-5 mb-10 text-center">
+                        Comienza agregando pacientes. {''}
+                        <span className="text-indigo-600 font-bold text-xl">Los verás de este lado.</span>
+                    </p>
+                </>
+            ) }
 
         </div>
 
